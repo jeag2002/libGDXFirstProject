949b41d3f838f95a2c5c4873ea05de2c600836c5
joan.alcaraz <joan.alcaraz@edreamsodigeo.com>
2020-08-05 23:18:47 +0200

    Second Test New Enemy + Shader Sepia City II

--- SecondTest/core/src/com/mygdx/game/enums/LevelEnum.java
+++ SecondTest/core/src/com/mygdx/game/enums/LevelEnum.java
@@ -4,31 +4,33 @@ import com.mygdx.game.logic.*;
 
 public enum LevelEnum {
 	
-	DESERT(0,"LEVEL 1",GameLogicInformation.DESERT_LEVEL, GameLogicInformation.NO_LIGHTS, 100, 100, 4),
-	JUNGLE(1,"LEVEL 2",GameLogicInformation.JUNGLE_LEVEL, GameLogicInformation.NO_LIGHTS, 100, 100, 4),
-	WINTER(2,"LEVEL 3",GameLogicInformation.WINTER_LEVEL, GameLogicInformation.NO_LIGHTS,100,0,4),
-	VOLCANO(3,"LEVEL 4",GameLogicInformation.VOLCANO_LEVEL, GameLogicInformation.NO_LIGHTS,100,100,4),
-	SPACE(4,"LEVEL 5", GameLogicInformation.SPACE_LEVEL, GameLogicInformation.NO_LIGHTS,100,100,4),
-	CITY(5,"LEVEL 6", GameLogicInformation.CITY_LEVEL, GameLogicInformation.NO_LIGHTS,100,100,4),
-	DESERT_LIGHTS(6,"LEVEL 7",GameLogicInformation.DESERT_LEVEL, GameLogicInformation.LIGHTS,100,100,4),
-	FABRIC(7,"LEVEL 8",GameLogicInformation.FABRIC_LEVEL,GameLogicInformation.NO_LIGHTS,100,100,4),
-	VOLCANO_LIGHTS(8,"LEVEL 9",GameLogicInformation.VOLCANO_LEVEL, GameLogicInformation.LIGHTS,100,100,4),
-	FABRIC_LIGHTS(9,"LEVEL 10",GameLogicInformation.FABRIC_LEVEL, GameLogicInformation.LIGHTS,100,100,4);
+	DESERT(0,"LEVEL 1",GameLogicInformation.DESERT_LEVEL, GameLogicInformation.NO_LIGHTS, 100, 100, 100, 4),
+	JUNGLE(1,"LEVEL 2",GameLogicInformation.JUNGLE_LEVEL, GameLogicInformation.NO_LIGHTS, 100, 100, 100, 4),
+	WINTER(2,"LEVEL 3",GameLogicInformation.WINTER_LEVEL, GameLogicInformation.NO_LIGHTS, 100, 100,0,4),
+	VOLCANO(3,"LEVEL 4",GameLogicInformation.VOLCANO_LEVEL, GameLogicInformation.NO_LIGHTS, 100,100,100,4),
+	SPACE(4,"LEVEL 5", GameLogicInformation.SPACE_LEVEL, GameLogicInformation.NO_LIGHTS,100,100,100,4),
+	CITY(5,"LEVEL 6", GameLogicInformation.CITY_LEVEL, GameLogicInformation.NO_LIGHTS,100, 100,100,4),
+	DESERT_LIGHTS(6,"LEVEL 7",GameLogicInformation.DESERT_LEVEL, GameLogicInformation.LIGHTS,100,100,100,4),
+	FABRIC(7,"LEVEL 8",GameLogicInformation.FABRIC_LEVEL,GameLogicInformation.NO_LIGHTS,100, 100,100,4),
+	VOLCANO_LIGHTS(8,"LEVEL 9",GameLogicInformation.VOLCANO_LEVEL, GameLogicInformation.LIGHTS,100, 100,100,4),
+	FABRIC_LIGHTS(9,"LEVEL 10",GameLogicInformation.FABRIC_LEVEL, GameLogicInformation.LIGHTS,100, 100,100,4);
 	
 	private int index;
 	private String LevelStr;
 	private int type;
 	private int lights;
+	private int numTurrets;
 	private int numDrons;
 	private int numMines;
 	private int numTanks;
 
 
-	private LevelEnum(int index, String levelStr, int type, int lights, int numDrons, int numMines, int numTanks) {
+	private LevelEnum(int index, String levelStr, int type, int lights, int numTurrets, int numDrons, int numMines, int numTanks) {
 		this.index = index;
 		LevelStr = levelStr;
 		this.type = type;
 		this.lights = lights;
+		this.numTurrets = numTurrets;
 		this.numDrons = numDrons;
 		this.numMines = numMines;
 		this.numTanks = numTanks;
@@ -52,9 +54,12 @@ public enum LevelEnum {
 		return LEnum;
 	}
 	
-	
-	
-	
+	public int getNumTurrets() {
+		return numTurrets;
+	}
+	public void setNumTurrets(int numTurrets) {
+		this.numTurrets = numTurrets;
+	}
 	public int getIndex() {
 		return index;
 	}
